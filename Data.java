8be90a4e24edb87fc2b39cb9fbcc552d936856e8
Teodor Popescu <teodor.popescu99@gmail.com>
2020-02-20 20:47:53 +0200

    Data class

---
+++ Data.java
@@ -0,0 +1,8 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class FileReader
+{
+  public List<Integer> library = new ArrayList<>();
+  public List<Integer> bookValues = new ArrayList<>();
+}
